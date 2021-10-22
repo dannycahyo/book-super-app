cab540672fca3ece39e4d13b72e5552936f38d83
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-10-22 10:14:34 +0700

    fix-header or navbar

--- containers/Header.tsx
+++ containers/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "./Logo";
-import { Layout, Menu, Typography } from "antd";
+import { Layout, Menu, Typography, Row, Col } from "antd";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -11,46 +11,86 @@ const Header = () => {
   return (
     <Header
       style={{
-        display: "flex",
-        justifyContent: "space-between",
-        marginBottom: 12,
+        marginBottom: 16,
+        position: "fixed",
+        zIndex: 1,
+        width: "100%",
       }}
     >
-      <Logo />
-      <Menu theme="dark" mode="horizontal" selectedKeys={[router.asPath]}>
-        <Menu.Item key="/mybooks">
-          <Link href="/mybooks" passHref>
-            <a>
-              <Typography.Title
-                level={5}
-                style={{
-                  color: "lightblue",
-                  marginTop: 20,
-                  marginBottom: 20,
-                }}
-              >
-                My Books
-              </Typography.Title>
-            </a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="/upcomingbooks">
-          <Link href="/upcomingbooks" passHref>
-            <a>
-              <Typography.Title
-                level={5}
-                style={{
-                  color: "lightblue",
-                  marginTop: 20,
-                  marginBottom: 20,
-                }}
-              >
-                Up Coming Books
-              </Typography.Title>
-            </a>
-          </Link>
-        </Menu.Item>
-      </Menu>
+      <Row justify="space-between">
+        <Col>
+          <Logo />
+        </Col>
+        <Col>
+          <Menu theme="dark" mode="horizontal" selectedKeys={[router.asPath]}>
+            <Menu.Item key="/mybooks">
+              <Link href="/mybooks" passHref>
+                <a>
+                  <Typography.Title
+                    level={5}
+                    style={{
+                      color: "lightblue",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    My Books
+                  </Typography.Title>
+                </a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="/upcomingbooks">
+              <Link href="/upcomingbooks" passHref>
+                <a>
+                  <Typography.Title
+                    level={5}
+                    style={{
+                      color: "lightblue",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    Upcoming
+                  </Typography.Title>
+                </a>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="/recommendation">
+              <Link href="/recommendation" passHref>
+                <a>
+                  <Typography.Title
+                    level={5}
+                    style={{
+                      color: "lightblue",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    Recommendation
+                  </Typography.Title>
+                </a>
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item key="/profile">
+              <Link href="/profile" passHref>
+                <a>
+                  <Typography.Title
+                    level={5}
+                    style={{
+                      color: "lightblue",
+                      marginTop: 20,
+                      marginBottom: 20,
+                    }}
+                  >
+                    Profile
+                  </Typography.Title>
+                </a>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Col>
+      </Row>
     </Header>
   );
 };
--- containers/Logo.tsx
+++ containers/Logo.tsx
@@ -5,23 +5,24 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/" passHref>
-      <a>
-        <Typography.Title
-          level={4}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            color: "lightblue",
-            marginTop: 16,
-            marginLeft: 40,
-          }}
-        >
-          <BookTwoTone style={{ marginRight: 2 }} />
-          <span>Book App</span>
-        </Typography.Title>
-      </a>
-    </Link>
+    <div style={{ float: "left", marginRight: 20 }}>
+      <Link href="/" passHref>
+        <a>
+          <Typography.Title
+            level={4}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "lightblue",
+              marginTop: 16,
+            }}
+          >
+            <BookTwoTone style={{ marginRight: 2 }} />
+            <span>Book App</span>
+          </Typography.Title>
+        </a>
+      </Link>
+    </div>
   );
 };
 
