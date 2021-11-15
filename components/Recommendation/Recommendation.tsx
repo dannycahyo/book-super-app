b20d3f93bb6245b9394f6a4d89209a555f72c26e
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-11-15 13:10:07 +0700

    finishing-search-feature-on-recommendation-page

--- components/Recommendation/Recommendation.tsx
+++ components/Recommendation/Recommendation.tsx
@@ -11,6 +11,7 @@ import {
   Row,
   Input,
   Select,
+  Result,
 } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { BestSellerBook } from "@type/Book";
@@ -22,6 +23,8 @@ const Recommendation = () => {
       {text}
     </Space>
   );
+
+  const [searchValue, setSearchValue] = useState<string>("");
 
   const [books, setBooks] = useState<BestSellerBook[]>([]);
 
@@ -43,6 +46,12 @@ const Recommendation = () => {
     "middle-grade-paperback-monthly",
     "young-adult-paperback-monthly",
   ];
+
+  const filteredBooks = books?.filter((book) => {
+    return book.title
+      .toLocaleLowerCase()
+      .includes(searchValue.toLocaleLowerCase());
+  });
 
   useEffect(() => {
     async function getRecommendationBook() {
@@ -99,78 +108,86 @@ const Recommendation = () => {
             placeholder="Find Your Book!"
             allowClear
             size="large"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchValue(event.target.value)
+            }
+            value={searchValue}
           />
         </Col>
       </Row>
-      <List
-        grid={{
-          gutter: 42,
-          xs: 1,
-          sm: 1,
-          md: 2,
-          lg: 3,
-          xl: 4,
-          xxl: 5,
-        }}
-        pagination={{
-          onChange: (page) => {
-            console.log(page);
-          },
-          responsive: true,
-          position: "bottom",
-          pageSize: 8,
-        }}
-        dataSource={books}
-        renderItem={(book) => (
-          <List.Item>
-            <Card
-              actions={[
-                <IconText
-                  icon={UserOutlined}
-                  text={book.author}
-                  key={book.title}
-                />,
-                <Button
-                  style={{
-                    background: "#3182CE",
-                    color: "white",
-                    fontWeight: "bold",
-                  }}
-                  key={book.title}
-                  onClick={() => {}}
-                >
-                  Buy This Book
-                </Button>,
-                <IconText
-                  icon={UserOutlined}
-                  text={book.price}
-                  key={book.title}
-                />,
-              ]}
-              cover={
-                <Image
-                  src={book.book_image}
-                  alt="Book Image"
-                  style={{ height: 320, width: 400 }}
-                  preview={false}
+      {filteredBooks?.length === 0 ? (
+        <Result status="404" title="404" subTitle="Can't Find Your Book" />
+      ) : (
+        <List
+          grid={{
+            gutter: 42,
+            xs: 1,
+            sm: 1,
+            md: 2,
+            lg: 3,
+            xl: 4,
+            xxl: 5,
+          }}
+          pagination={{
+            onChange: (page) => {
+              console.log(page);
+            },
+            responsive: true,
+            position: "bottom",
+            pageSize: 8,
+          }}
+          dataSource={filteredBooks}
+          renderItem={(book) => (
+            <List.Item>
+              <Card
+                actions={[
+                  <IconText
+                    icon={UserOutlined}
+                    text={book.author}
+                    key={book.title}
+                  />,
+                  <Button
+                    style={{
+                      background: "#3182CE",
+                      color: "white",
+                      fontWeight: "bold",
+                    }}
+                    key={book.title}
+                    onClick={() => {}}
+                  >
+                    Buy This Book
+                  </Button>,
+                  <IconText
+                    icon={UserOutlined}
+                    text={book.price}
+                    key={book.title}
+                  />,
+                ]}
+                cover={
+                  <Image
+                    src={book.book_image}
+                    alt="Book Image"
+                    style={{ height: 320, width: 400 }}
+                    preview={false}
+                  />
+                }
+                hoverable
+              >
+                <Card.Meta
+                  avatar={
+                    <Avatar src="https://image.pngaaa.com/538/21538-middle.png" />
+                  }
+                  title={
+                    <Typography.Title style={{ color: "#3182CE" }} level={5}>
+                      {book.title}
+                    </Typography.Title>
+                  }
                 />
-              }
-              hoverable
-            >
-              <Card.Meta
-                avatar={
-                  <Avatar src="https://image.pngaaa.com/538/21538-middle.png" />
-                }
-                title={
-                  <Typography.Title style={{ color: "#3182CE" }} level={5}>
-                    {book.title}
-                  </Typography.Title>
-                }
-              />
-            </Card>
-          </List.Item>
-        )}
-      />
+              </Card>
+            </List.Item>
+          )}
+        />
+      )}
     </Col>
   );
 };
